4576b54cb6484b8c03d01553be24d93941012ec7
Gustavo Carvalho <prercursor@gmail.com>
2024-10-20 21:10:43 -0300

    feat: set vertical cursor blinking in edit mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+-- Set vertical bar cursor in insert mode and configure blinking
+vim.api.nvim_create_autocmd("InsertEnter", {
+  pattern = "*",
+  command = "set guicursor+=i:ver25-blinkon500",
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set guicursor-=i:ver25-blinkon500",
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,3 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-
--- make cursor blink in edit mode
-vim.opt.guicursor = "i:block-blinkwait1000-blinkon500-blinkoff500"
